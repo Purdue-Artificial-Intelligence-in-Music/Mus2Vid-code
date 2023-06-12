8ae0f8e091bf8b564ee5d1c028e0439ca36bb619
Tanin Padungkirtsakul <tpadungk@purdue.edu>
2023-06-12 11:56:42 -0400

    updated diffusion model

--- Prototype/utils/diffusion.py
+++ Prototype/utils/diffusion.py
@@ -1,17 +1,20 @@
-from diffusers import StableDiffusionPipeline
-stable_diffusion_model_id = "runwayml/stable-diffusion-v1-5"
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+stable_diffusion_model_id = "stabilityai/stable-diffusion-2-1-base"
 import torch
 
 def get_pipe():
-    pipe = StableDiffusionPipeline.from_pretrained(stable_diffusion_model_id, torch_dtype=torch.float16)
+    pipe = DiffusionPipeline.from_pretrained(stable_diffusion_model_id, torch_dtype=torch.float16, revision="fp16")
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
     pipe = pipe.to("cuda")
     return pipe
 
-def get_pic(prompt, inference = 50, guidance_scale = 7.5, num_images_per_prompt = 1):
+def get_pic(prompt,negative_prompt = "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, mutation, mutated, extra limbs, extra legs, extra arms, disfigured, deformed, cross-eye, body out of frame, blurry, bad art, bad anatomy, blurred, text, watermark, grainy"
+            , inference = 10, guidance_scale = 7.5, num_images_per_prompt = 1):
     """
     Creates an image using stable diffusion pipeline
     Parameters:
         prompt: string of the prompt
+        negative prompt: string of the negative prompt
         inference: number of inference step, around 50 for a high quality image
         guidance scale: a way to increase the adherence to the conditional signal that guides the generation (text, in this case) as well as overall sample quality
     Returns:
@@ -23,6 +26,7 @@ def get_pic(prompt, inference = 50, guidance_scale = 7.5, num_images_per_prompt 
     pipe = get_pipe()
     return pipe(
         prompt,
+        negative_prompt= negative_prompt,
         num_inference_steps = inference,
         guidance_scale = guidance_scale,
         num_images_per_prompt = num_images_per_prompt,
