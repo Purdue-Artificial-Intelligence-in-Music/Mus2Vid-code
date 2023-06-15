4cc789edcc941709deba5837d014f3532e9e31c0
unknown <timmynadolsky@gmail.com>
2023-06-15 14:21:42 -0400

    Cleaned up chord detection methods

--- Prototype/parent.py
+++ Prototype/parent.py
@@ -2,13 +2,13 @@ import sys
 from utils.midi import *
 from utils.diffusion import *
 from utils.prompting import *
+model = tf.keras.models.load_model('utils\model.h5')
 
 def generate_picture(midi_path, image_name):
     midi_obj = predict(midi_path)
     midi_features = get_features(midi_obj)
     print(midi_features.shape)
 
-    model = tf.keras.models.load_model('utils\model.h5')
     subgenre_num = model.predict(midi_features)
     print(subgenre_num)
     subgenre = get_genre(np.argmax(subgenre_num))
