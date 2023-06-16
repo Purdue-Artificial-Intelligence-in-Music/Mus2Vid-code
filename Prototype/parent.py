722b307373a2b4460d48127b042e6826047b673c
JPKamphuis <jpkamphu@purdue.edu>
2023-06-16 14:28:45 -0400

    wav file changes

--- Prototype/parent.py
+++ Prototype/parent.py
@@ -23,7 +23,8 @@ def generate_picture(midi_path, image_name):
 
 def main():
     # generate_picture(sys.argv[1], sys.argv[2])
-    generate_picture('test_mp3/vivaldi_winter.mp3', 'image.png')
+    generate_picture('..\Genre_NN\maestro-v3.0.0-wav\maestro-v3.0.0\\2004\MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.wav', 'image.png')
+    
 
 if __name__ == "__main__":
     main()
