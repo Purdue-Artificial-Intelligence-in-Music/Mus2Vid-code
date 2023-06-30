7159ed70b1c426c0713d21ad31fa04822a610c48
JPKamphuis <jpkamphu@purdue.edu>
2023-06-30 13:34:07 -0400

    beginning feature selection

--- Prototype/utils/prompting.py
+++ Prototype/utils/prompting.py
@@ -108,22 +108,15 @@ def get_emotion(emotion_input):
     return emotion_prompt
 
 def perspectiveRandom():
-    modifiers = []
-    angle_modifier = ["extreme close-up", "close-up", "medium shot,waist and torso shot", "long shot, wide shot, full body", "extreme long shot, from a distance"]
-    lens_modifier = ["fast shutter speed, 1/1000 sec shutter","slow shutter speed, long exposure","tilt shift photography", "macro lens, macro photo,Sigma 105mm f/2.8", "wide angle lens, 15mm, ultra-wide shot", "4k"]
-    artstyle_modifier = ["woodcut painting", "charcoal sketch","watercolor","acrylic on canvas","colored pencil,detailed","oil painting","airbrush", "digital painting","low poly, unreal engine,Blender render","isometric 3D, highest quality render","claymation"]
-    scale_modifier = ["grand","high-scale","monumental","imposing","delicate","elegant","intricate","decorative","opulent","ornamental"]
+    angle_modifier = ["extreme close-up", "close-up", "medium shot", "waist and torso shot", "long shot", "wide shot", "full body", "extreme long shot", "from a distance"]
+    lens_modifier = ["fast shutter speed", "1/1000 sec shutter","slow shutter speed", "long exposure","tilt shift photography", "macro lens", "macro photo", "Sigma 105mm f/2.8", "wide angle lens", "ultra-wide shot", "4k"]
+    artstyle_modifier = ["woodcut painting", "charcoal sketch","watercolor","acrylic on canvas","colored pencil", "oil painting", "airbrush", "digital painting","low poly,unreal engine,Blender render","isometric 3D, highest quality render"]
 
     angle_random = random.choice(angle_modifier)
     lens_random = random.choice(lens_modifier)
     artstyle_random = random.choice(artstyle_modifier)
-    scale_random = random.choice(scale_modifier)
-    
-    modifiers.append(angle_random)
-    modifiers.append(lens_random)
-    modifiers.append(artstyle_random)
-    modifiers.append(scale_random)
-    return modifiers
+
+    return [angle_random, lens_random, artstyle_random]
 
 ## function for different type of genre with date
 def get_genre(subgenre):
