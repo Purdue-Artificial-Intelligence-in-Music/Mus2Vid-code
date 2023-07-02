e3e8c1718db58f97f0dd0ec2c5631798ac797a16
realkevinkwon <kevinkwonmail@gmail.com>
2023-07-02 09:11:46 -0500

    rename functions

--- src/data/get.py
+++ src/data/get.py
@@ -2,14 +2,20 @@ import pandas as pd
 
 ANNOTATIONS_PATH = "./data/processed/annotations/annotations averaged per song/song_level/static_annotations_averaged_songs_1_2000.csv"
 
-def get_data():
-    dataframe = pd.read_csv(ANNOTATIONS_PATH)
-    song_id_list = dataframe["song_id"]
-    targets = dataframe[["valence_mean", "arousal_mean"]]
+def get_song_id_list():
+    df = pd.read_csv(ANNOTATIONS_PATH)
+    song_id_list = df["song_id"]
+    
+    return song_id_list
 
-    return song_id_list, targets
+def get_targets():
+    df = pd.read_csv(ANNOTATIONS_PATH)
+    targets = df[["valence_mean", "arousal_mean"]]
+
+    return targets
 
 if __name__ == "__main__":
-    song_id_list, targets = get_data()
+    song_id_list, targets = get_song_id_list(), get_targets()
+
     print(song_id_list)
     print(targets)
