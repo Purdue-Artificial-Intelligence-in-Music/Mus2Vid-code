c1a41977a3b470507f52c46374d454749d788f09
realkevinkwon <kevinkwonmail@gmail.com>
2023-07-03 00:10:44 -0400

    rename package to emotion

--- emotion/model/regression.py
+++ emotion/model/regression.py
@@ -3,7 +3,14 @@ from sklearn import svm
 class EmotionClassifier():
 
     def __init__(self):
+        self.
         pass
 
     def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def save(self):
         pass
