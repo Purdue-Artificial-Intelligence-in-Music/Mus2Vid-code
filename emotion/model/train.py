5661c6d8c6cb5accb69fec9467a4733919eb143d
JPKamphuis <jpkamphu@purdue.edu>
2023-07-05 14:37:05 -0400

    update

--- emotion/model/train.py
+++ emotion/model/train.py
@@ -4,8 +4,8 @@ from utilities import get_features, get_valence_targets, get_arousal_targets
 def train():
     valence_features = get_features("valence_features")
     arousal_features = get_features("arousal_features")
-    valence_targets = get_valence_targets("valence_targets")
-    arousal_targets = get_arousal_targets("arousal_targets")
+    valence_targets = get_valence_targets()
+    arousal_targets = get_arousal_targets()
 
     valence_regressor, arousal_regressor = EmotionRegressor(), EmotionRegressor()
 
--- emotion/model/utilities.py
+++ emotion/model/utilities.py
@@ -1,5 +1,7 @@
 import joblib
+import pandas as pd
 
+ANNOTATIONS_PATH = "./data/processed/annotations/annotations averaged per song/song_level/static_annotations_averaged_songs_1_2000.csv"
 FEATURES_PATH = "./data/processed/features/"
 TARGETS_PATH = "./data/processed/targets/"
 FEATURES_EXT = ".features"
@@ -8,11 +10,17 @@ TARGETS_EXT = ".targets"
 def get_features(filename):
     return joblib.load(FEATURES_PATH + filename + FEATURES_EXT)
 
-def get_valence_targets(filename):
-    return joblib.load(TARGETS_PATH + filename + TARGETS_EXT)
+def get_valence_targets():
+    df = pd.read_csv(ANNOTATIONS_PATH)
+    targets = df["valence_mean"]
 
-def get_arousal_targets(filename):
-    return joblib.load(TARGETS_PATH + filename + TARGETS_EXT)
+    return targets
+
+def get_arousal_targets():
+    df = pd.read_csv(ANNOTATIONS_PATH)
+    targets = df["arousal_mean"]
+
+    return targets
 
 if __name__ == "__main__":
     pass
