5868c9297e114c7f16dc5ee33ecf8dbf7472fe93
realkevinkwon <kevinkwonmail@gmail.com>
2023-07-06 17:14:27 -0400

    rename constants

--- src/emotion/model/utilities.py
+++ src/emotion/model/utilities.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 
 ANNOTATIONS_PATH = "./data/processed/annotations/annotations averaged per song/song_level/static_annotations_averaged_songs_1_2000.csv"
-FEATURES_PATH = "./data/processed/features/"
-TARGETS_PATH = "./data/processed/targets/"
-FEATURES_EXT = ".features"
-TARGETS_EXT = ".targets"
+FEATURES_DIR = "./data/processed/features"
+TARGETS_DIR = "./data/processed/targets"
+FEATURES_EXT = "features"
+TARGETS_EXT = "targets"
 
 
 def get_features(filename):
-    return joblib.load(FEATURES_PATH + filename + FEATURES_EXT)
+    return joblib.load(f"{FEATURES_DIR}/{filename}.{FEATURES_EXT}")
 
 def get_valence_targets():
     df = pd.read_csv(ANNOTATIONS_PATH)
